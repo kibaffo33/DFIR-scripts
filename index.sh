47ff1f9aa25f9d9eef49678c5971f11f9024a24f
kibaffo33 <robertedwardwilliams@me.com>
2022-04-12 20:56:37 +0100

    Adds hashing

--- index.sh
+++ index.sh
@@ -22,3 +22,8 @@ egrep -f keywords.txt index.txt | sort > keywords-filenames.txt
 egrep '[0-9]{13}' . | sort > keywords-timestamps-13.txt
 egrep '[0-9]{10}' . | sort > keywords-timestamps-10.txt
 wc -l keywords-*.txt | sort -rn > keywords-stats.txt
+
+# Hashing
+cat index.txt | sed 's/^/md5 -r /' | bash > hashes-md5.txt
+cat index.txt | sed 's/^/shasum -a 1 /' | bash > hashes-sha1.txt
+cat index.txt | sed 's/^/shasum -a 256 /' | bash > hashes-sha256.txt
